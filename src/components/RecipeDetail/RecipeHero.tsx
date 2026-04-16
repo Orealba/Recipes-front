8d45150e6545b5cbd4a87f46ce52c89490fc7d9f
Orealba Soriano <orealbas@gmail.com>
2026-04-16 17:51:52 +0200

    feat: Add RecipeInfo component and fix RecipeDetail again

--- src/components/RecipeDetail/RecipeHero.tsx
+++ src/components/RecipeDetail/RecipeHero.tsx
@@ -1,16 +1,16 @@
 interface RecipeHeroProps {
   image_url: string | null;
   name: string;
-  description: string | null;
+  description?: string | null;
 
 }
 
-export function RecipeHero({image_url, name, description}: RecipeHeroProps) {
+export function RecipeHero({image_url, name}: RecipeHeroProps) {
   return (
-    <div className="relative  ">
-      <img src={image_url || 'https://img.hellofresh.com/f_auto,fl_lossy,h_640,q_auto,w_1200/hellofresh_s3/image/HF_Y23_R08_W32_ES_ESCFVQV0094-4_Main_R_high-3feb0424.jpg'} alt={name} className="w-full h-80 object-cover" />
+    <div className="max-w-5xl mx-auto relative">
+      <img src={image_url ? `/hellofresh_images/${image_url}` : '...'} alt={name} className="w-full h-80 object-cover" />
       <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/60 to-transparent p-6">
-      <h1 className="text-3xl font-bold text-white mb-2 text-center inline-block bg-black/40 px-4 py-2 rounded-lg">{name}</h1>
+      <h1 className="text-3xl font-bold text-white   text-center inline-block bg-black/20 px- py-2 rounded-lg">{name}</h1>
 
       </div>
 
--- src/pages/RecipeDetail.tsx
+++ src/pages/RecipeDetail.tsx
@@ -1,54 +1,46 @@
 import { RecipeHero } from "../components/RecipeDetail/RecipeHero";
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { supabase } from "../lib/supabase";
+import { RecipeInfo } from '../components/RecipeDetail/RecipeInfo';
+import { recipes } from '../data/recipes'
 import { RecipeDescription } from '../components/RecipeDetail/RecipeDescription';
-
 interface Recipe {
   id: string;
   name: string;
-  image_url: string | null;
-  description: string | null;
-
+  description: string;
+  local_image_name: string;
+  total_time: string;
+  recipe_yield: number;
+  difficulty: number;
+  rich_ingredients: any[];
+  ingredients_text: string[];
+  instruction_steps: any[];
+  instructions: string;
+  utensils: any[];
+  allergens: any[];
+  nutrition: any;
+  tags: any[];
+  cuisines: any[];
 }
-
 export function RecipeDetail() {
-  const {id} = useParams();
-  const [recipe, setRecipe] = useState<Recipe | null>(null);
+  const { id } = useParams<{ id: string }>();
 
-  useEffect(() => {
-    const fetchRecipe = async () => {
-      if (!id) return;
-
-      const {data, error} =await supabase
-      .from('recipes')
-      .select('id, name, image_url, description')
-      .eq('id', id)
-      .single();
-
-      console.log('data:', data);
-      console.log('error:', error);
-      if (error) {
-        console.error('Error fetching recipe', error);
-        return
-      }
-      setRecipe(data);
-
-    };
-    fetchRecipe()
-  }, [id]);
-  if (!recipe){
+  const recipe = id ? recipes.find(r => r.id === id) : null;
+  if (!recipe) {
     return <div className="p-4">Cargando...</div>;
   }
-
   return (
-    <div className="mx-w-4xl max-auto">
+    <div className="max-w-5xl mx-auto px-6">
       <RecipeHero
-      image_url={recipe.image_url}
-  name={recipe.name}
-  description={recipe.description}
-    ></RecipeHero>
-    <RecipeDescription description={recipe.description} />
+        image_url={recipe.local_image_name}
+        name={recipe.name}
+        description={null}
+      />
+      <RecipeInfo
+  total_time={recipe.total_time}
+  calories={recipe.nutrition?.calories}
+  difficulty={recipe.difficulty}
+/>
+      <RecipeDescription description={recipe.description} />
     </div>
   )
 }
